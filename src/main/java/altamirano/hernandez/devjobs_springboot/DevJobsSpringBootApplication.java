43ff3b13370d4d46906a64c9ab33d68498651233
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-05-13 17:14:17 -0600

    initial commit

---
+++ src/main/java/altamirano/hernandez/devjobs_springboot/DevJobsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.devjobs_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevJobsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevJobsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/altamirano/hernandez/devjobs_springboot/DevJobsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.devjobs_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJobsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
