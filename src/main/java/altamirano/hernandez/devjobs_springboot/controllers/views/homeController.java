83ffa89462463214c092a2aefcac0f4a78b8911a
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-05-13 18:29:50 -0600

    feat: add base layout and first-look formatting

---
+++ src/main/java/altamirano/hernandez/devjobs_springboot/controllers/views/homeController.java
@@ -0,0 +1,22 @@
+package altamirano.hernandez.devjobs_springboot.controllers.views;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class homeController {
+
+    @GetMapping("/")
+    public String layoutBase(Model model){
+        model.addAttribute("nombrePagina", "DevJobs");
+        model.addAttribute("tagline", "Encuentra y publica trabajos para desarrolladores web");
+        model.addAttribute("barra", true);
+        model.addAttribute("boton", true);
+
+        return "layout/layout";
+    }
+
+}
